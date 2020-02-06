f3d935a39930c4edcb7ed438f691be554fc7d72e
mbacke <elhadjimbackembaye@gmail.com>
2020-02-06 08:58:19 +0000

    first commit

---
+++ js/quizz.js
@@ -0,0 +1,132 @@
+
+// $(document).ready(function(){
+//     var points=0;
+// var titrepage=document.getElementById('titrepage');
+// var fisrtquestion=document.getElementById('question1');
+// var secondquestion=document.getElementById('question2');
+// var thirdquestion=document.getElementById('question3');
+// var fourthquestion=document.getElementById('question4');
+// var avantresultat=document.getElementById('avantresultat');
+// var formulaire=document.getElementById('formulaire');
+// var apresenregistrement=document.getElementById('apresenregistrement');
+// var resultatquestion=document.getElementById('resultatquestion');
+// var vuebonnereponse=document.getElementById('repartition');
+// var linketpartage=document.getElementById('linketpartage');
+// var prenom;
+// var nom;
+// var telephone;
+//     // $.get("page/save.php?donner=visiteur",function(){
+//     //     alert("bravo ajout succes")
+//     // })
+//        // fisrtquestion.style.display="none";
+//         secondquestion.style.display="none";
+//         thirdquestion.style.display="none";
+//         fourthquestion.style.display="none";
+//         avantresultat.style.display="none";
+//         formulaire.style.display="none";
+//         apresenregistrement.style.display="none";
+//         resultatquestion.style.display="none";
+//         vuebonnereponse.style.display="none";
+//         linketpartage.style.display="none";
+// })
+
+
+
+//     function good1(){
+       
+//   //  fisrtquestion.style.display="none";
+//     secondquestion.style.display="block";
+//     points++;
+//     alert("good");
+//     };
+//     function bad1(){
+        
+//     //fisrtquestion.style.display="none";
+//     secondquestion.style.display="block";
+//     alert("bad");
+//     }
+// // function good2(){
+// // secondquestion.style.display="none";
+// // thirdquestion.style.display="block";
+// // points++;
+// // $.get("page/save.php?donner=michemin",function(){
+// //     alert("bravo ajout succes")
+// // })
+// // }
+// // function bad2(){
+// // secondquestion.style.display="none";
+// // thirdquestion.style.display="block";
+// // $.get("page/save.php?donner=michemin",function(){
+// //     alert("bravo ajout succes")
+// // })
+// // }
+// // function good3(){
+// // thirdquestion.style.display="none";
+// // fourthquestion.style.display="block";
+// // points++;
+// // }
+// // function bad3(){
+// // thirdquestion.style.display="none";
+// // fourthquestion.style.display="block";
+// // }
+// // function good4(){
+// // fourthquestion.style.display="none";
+// // avantresultat.style.display="block";
+// // titrepage.style.display="none";
+// // points++;
+// // document.getElementById('cpt1').innerHTML=points;
+// // document.getElementById('cpt2').innerHTML=points;
+// // document.getElementById('cpt3').innerHTML=points;
+// // }
+// // function bad4(){
+// // fourthquestion.style.display="none";
+// // avantresultat.style.display="block";
+// // titrepage.style.display="none";
+// // document.getElementById('cpt1').innerHTML=points;
+// // document.getElementById('cpt2').innerHTML=points;
+// // document.getElementById('cpt3').innerHTML=points;
+// // }
+// // function recommancer(){
+// // fisrtquestion.style.display="block"
+// // secondquestion.style.display="none";
+// // thirdquestion.style.display="none";
+// // fourthquestion.style.display="none";
+// // avantresultat.style.display="none";
+// // formulaire.style.display="none";
+// // apresenregistrement.style.display="none";
+// // resultatquestion.style.display="none";
+// // vuebonnereponse.style.display="none";
+// // linketpartage.style.display="none";
+// // }
+// // function jerejoint(){
+// // formulaire.style.display="block";
+// // resultatquestion.style.display="none";
+// // }
+// // function jenerejointplus(){
+// // formulaire.style.display="none";
+// // resultatquestion.style.display="block";
+// // }
+// // function ouipartage(){
+// // linketpartage.style.display="block";
+// // vuebonnereponse.style.display="none";
+// // }
+// // function nonpartage(){
+// // vuebonnereponse.style.display="block";
+// // }
+// // function jevalide(){
+// // apresenregistrement.style.display="block";
+// // prenom=document.getElementById('valeur1').value;
+// // nom=document.getElementById('valeur2').value;
+// // telephone=document.getElementById('valeur3').value;
+// // document.getElementById('prenomsave1').innerHTML=prenom;
+// // document.getElementById('prenomsave2').innerHTML=prenom;
+// // $.get("db.php?prenom="+prenom+"&nom="+nom+"&telephone="+telephone,function(){
+// //     alert("bravo ajout succes")
+// // })
+// // $.get("page/save.php?donner=inscrit",function(){
+// //     alert("bravo ajout succes")
+// // })
+// // }
+// // function jesaute(){
+// // resultatquestion.style.display="block";
+// // }
